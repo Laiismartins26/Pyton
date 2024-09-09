d3c0daa34ff514d1025dc9b77f5d14faccc393cb
Laiismartins26 <93133593+Laiismartins26@users.noreply.github.com>
2024-09-09 11:01:16 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+#Tabuada
+
+contador = 0 
+numero = int(input("Digite um número: "))
+while (contador<=10):
+    resultado = numero * contador
+    print(f"{numero} x {contador} = {resultado}")
+    contador += 1

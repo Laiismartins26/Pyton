9ae1063b16749562e0650c17956ae95f1306c59f
Laiismartins26 <93133593+Laiismartins26@users.noreply.github.com>
2024-09-11 11:52:41 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,11 @@
+# Funções
+# Função soma
+
+def soma(a,b):
+    return a + b 
+ValorA = int(input("Digite um valor: "))
+ValorB = int(input("Digite um valor: "))
+
+resultado = soma(Valora, ValorB)
+
+print(f"O resultado da soma = {resultado}")
